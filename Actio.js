37694787d0565761cca65d589efbef2405d5518c
Krystalzxj <103553035+Krystalzxj@users.noreply.github.com>
2022-06-25 00:39:16 +0800

    Create Actio.js

---
+++ Actio.js
@@ -0,0 +1,36 @@
+/*
+ * 由@zZPiglet编写
+ * 原脚本地址：https://raw.githubusercontent.com/zZPiglet/Task/master/asset/flushDNS.js
+ * 由@Rabbit-Spec修改
+ * 更新日期：2022.06.17
+ * 版本：1.6
+ */
+
+!(async () => {
+    let panel = { title: "Flush DNS" },
+        showServer = true,
+        dnsCache;
+    if (typeof $argument != "undefined") {
+        let arg = Object.fromEntries($argument.split("&").map((item) => item.split("=")));
+        if (arg.title) panel.title = arg.title;
+        if (arg.icon) panel.icon = arg.icon;
+        if (arg.color) panel["icon-color"] = arg.color;
+        if (arg.server == "false") showServer = false;
+    }
+    if (showServer) {
+        dnsCache = (await httpAPI("/v1/dns", "GET")).dnsCache;
+        dnsCache = [...new Set(dnsCache.map((d) => d.server))].toString().replace(/,/g, "\n");
+    }
+    if ($trigger == "button") await httpAPI("/v1/dns/flush");
+    let delay = ((await httpAPI("/v1/test/dns_delay")).delay * 1000).toFixed(0);
+    panel.content = `𝘿𝙉𝙎延迟: ${delay}ms${dnsCache ? `\nserver:\n${dnsCache}` : ""}`;
+    $done(panel);
+})();
+
+function httpAPI(path = "", method = "POST", body = null) {
+    return new Promise((resolve) => {
+        $httpAPI(method, path, body, (result) => {
+            resolve(result);
+        });
+    });
+}
